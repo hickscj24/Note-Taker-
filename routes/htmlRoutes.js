305cfa19408387656730cc52f84d0f6a2226a895
Cody Hicks <hickscj5@gmail.com>
2022-01-22 10:23:01 -0700

    new route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,7 +8,7 @@ router.get('/notes', function (req, res){
 });
 
 
-router.get('*', function (req, res){
+router.get('/', function (req, res){
     res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
